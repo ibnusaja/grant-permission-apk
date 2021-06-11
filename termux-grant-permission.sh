c6686825dffd32f9e5959aa65facad84a63fac3b
ibnusaja <45093115+ibnusaja@users.noreply.github.com>
2021-06-11 21:01:36 +0800

    Add files via upload

---
+++ termux-grant-permission.sh
@@ -0,0 +1,14 @@
+pm grant com.termux com.termux.permission.RUN_COMMAND
+pm grant com.termux android.permission.ACCESS_NETWORK_STATE;
+pm grant com.termux android.permission.INTERNET;
+pm grant com.termux android.permission.WRITE_EXTERNAL_STORAGE;
+pm grant com.termux android.permission.WAKE_LOCK;
+pm grant com.termux android.permission.VIBRATE;
+pm grant com.termux android.permission.FOREGROUND_SERVICE;
+pm grant com.termux android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS;
+pm grant com.termux android.permission.SYSTEM_ALERT_WINDOW;
+pm grant com.termux android.permission.READ_LOGS;
+pm grant com.termux android.permission.DUMP;
+pm grant com.termux android.permission.WRITE_SECURE_SETTINGS;
+pm grant com.termux android.permission.REQUEST_INSTALL_PACKAGES;
+echo; echo 'selesai'
